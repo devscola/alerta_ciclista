f6de5b1b0b09157701ba81e4f93ad19106ad2cb9
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2015-12-08 10:59:58 +0000

    update migration scripts

--- db/migration_script.sql
+++ db/migration_script.sql
@@ -1,3 +1,3 @@
-select incident_title as title, concat(ip.person_first, ' ', ip.person_last) as author, person_email as email, incident_description as comment, latitude, longitude, location_name as address from incident i inner join incident_person ip on ip.incident_id=i.id inner join location l on l.id = i.location_id;
+select concat('insert into "suggestions" (title, author, email, comment, latitude, longitude, address, updated_at, created_at, visible) VALUES "(', incident_title, '","', concat(ip.person_first, ' ', ip.person_last), '","',person_email, '","',incident_description, '",',latitude, ',',longitude, ',"',location_name, '",',incident_date, ',',incident_date, ',',1,')') from incident i inner join incident_person ip on ip.incident_id=i.id inner join location l on l.id = i.location_id;
 
 select incident_id, concat('http://alertaciclista.com/media/uploads/',media_link) from media;
